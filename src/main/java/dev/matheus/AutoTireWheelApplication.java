56aa6d34fe47be134c923a50ad8a55b9e0d5c8ef
Matheus <rmatheusluiz@gmail.com>
2026-07-27 15:46:38 -0300

    first commit

---
+++ src/main/java/dev/matheus/AutoTireWheelApplication.java
@@ -0,0 +1,13 @@
+package dev.matheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTireWheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTireWheelApplication.class, args);
+	}
+
+}
