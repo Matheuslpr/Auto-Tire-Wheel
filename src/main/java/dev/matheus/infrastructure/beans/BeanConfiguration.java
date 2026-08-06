7fd069eda5969b9708376225d3a33598d7d2479c
Matheus <rmatheusluiz@gmail.com>
2026-08-06 17:51:31 -0300

    feat(config): adiciona beans dos usecases do fornecedores

--- src/main/java/dev/matheus/infrastructure/beans/BeanConfiguration.java
+++ src/main/java/dev/matheus/infrastructure/beans/BeanConfiguration.java
@@ -1,13 +1,16 @@
 package dev.matheus.infrastructure.beans;
 
 import dev.matheus.core.gateway.ClientesGateway;
+import dev.matheus.core.gateway.FornecedoresGateway;
 import dev.matheus.core.usecases.clientes.*;
+import dev.matheus.core.usecases.fornecedores.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfiguration {
 
+    // Beans for Clientes use cases
     @Bean
     public CadastrarClientesUseCase cadastrarClientesUseCase(ClientesGateway clientesGateway){
         return new CadastrarClientesUseCaseImpl(clientesGateway);
@@ -28,4 +31,30 @@ public class BeanConfiguration {
         return new ListarClientesUseCaseImpl(clientesGateway);
     }
 
+    // Beans for Fornecedores use cases
+
+    @Bean
+    public CadastrarFornecedoresUseCase cadastrarFornecedoresUseCase(FornecedoresGateway fornecedoresGateway){
+        return new CadastrarFornecedoresUseCaseImpl(fornecedoresGateway);
+    }
+
+    @Bean
+    public AtualizarFornecedoresUseCase atualizarFornecedoresUseCase(FornecedoresGateway fornecedoresGateway){
+        return new AtualizarFornecedoresUseCaseImpl(fornecedoresGateway);
+    }
+
+    @Bean
+    public BuscarFornecedoresUseCase buscarFornecedoresUseCase(FornecedoresGateway fornecedoresGateway){
+        return new BuscarFornecedoresUseCaseImpl(fornecedoresGateway);
+    }
+
+    @Bean
+    public ListarFornecedoresUseCase listarFornecedoresUseCase(FornecedoresGateway fornecedoresGateway) {
+        return new ListarFornecedoresUseCaseImpl(fornecedoresGateway);
+    }
+
+    @Bean
+    public DeletarFornecedoresUseCase deletarFornecedoresUseCase(FornecedoresGateway fornecedoresGateway) {
+        return new DeletarFornecedoresUseCaseImpl(fornecedoresGateway);
+    }
 }
